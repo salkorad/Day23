55ad45533f7c3fdafb2f565afd4049fc81ea051e
salkorad <salko.radovovic@gmail.com>
2021-03-30 22:14:14 +0200

    Changing the state object

---
+++ ex03/src/Plant.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class Plant extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { type: "Flowering plant" };
+  }
+
+  changeType = () => {
+    this.setState({ type: "Non flowering type" });
+  };
+  render() {
+    return (
+      <div>
+        <h2>Plant type is {this.state.type}</h2>
+        <button type="button" onClick={this.changeType}>
+          Change type
+        </button>
+      </div>
+    );
+  }
+}
+export default Plant;
